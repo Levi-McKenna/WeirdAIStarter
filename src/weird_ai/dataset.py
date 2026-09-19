f299fd3da22ac4180222d3be8ec3e7a13318f3f0
Levi-McKenna <lmckenna090@gmail.com>
2026-09-18 23:00:09 -0400

    tokenize: alter dataset objects
    Signed-off-by: Levi-McKenna <lmckenna090@gmail.com>

--- src/weird_ai/dataset.py
+++ src/weird_ai/dataset.py
@@ -1,17 +1,22 @@
+import math
+
 import torch
 from torch.utils.data import Dataset
 
-class LyricsDataset:
-    def __init__(self, tokens, block_size):
+
+class LyricsDataset(Dataset):
+    def __init__(self, tokens, block_size, stride):
         self.tokens = tokens
+        self.stride = stride
         self.block_size = block_size
 
     def __len__(self):
-        return len(self.tokens) - self.block_size
+        return math.ceil((len(self.tokens) - self.block_size) / self.stride)
 
     def __getitem__(self, index):
         # TODO:
         # Get the input/output token sequences
         # Calculate x by grabbing the sublist of tokens starting at the index up to the block_size
         # Calculate y by grabbing the sublist of tokens starting at index + 1 up to block_size + 1
-        return self.tokens[index:index+self.block_size], self.tokens[index+1:self.block_size+1]
+        start = index * self.stride
+        return torch.tensor(self.tokens[start:start+self.block_size]),torch.tensor(self.tokens[start+1:start+self.block_size+1])
--- tests/lyrics_dataset_tests.py
+++ tests/lyrics_dataset_tests.py
@@ -3,11 +3,9 @@ from weird_ai.dataset import LyricsDataset
 
 def test_lyrics_dataset_getitem():
     tokens = [1, 2, 3, 4, 5]
-    dataset = LyricsDataset(tokens, block_size=3)
+    dataset = LyricsDataset(tokens, block_size=3, stride=1)
 
     x, y = dataset[0]
 
-    # already a list: assert x.tolist() == [1, 2, 3]
-    # already a list: assert y.tolist() == [2, 3, 4]
-    assert x == [1, 2, 3]
-    assert y == [2, 3, 4]
+    assert list(x) == [1, 2, 3]
+    assert list(y) == [2, 3, 4]
